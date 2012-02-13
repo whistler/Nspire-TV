80a5e3a85c565b616e0442f8b51ce90342f995f6
Nima Gardideh <nima@halfmoon.ws>
2012-02-12 23:58:43 -0500

    First commit. Rails init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NspireTV::Application.config.secret_token = 'e0dd7c782b034e49884a196776dab3ab36b0e28451260c9f6ab6d64b68926cee32c7c581b6ba187c033f81f8bf8c931da5fbf6036bfb088cfc601348b37e182a'
